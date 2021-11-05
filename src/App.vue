6d942ffcf362c9dd83b51fa5793a66a7ddf6b1b6
Ekaterina Rykova <katrykova2002@yandex.ru>
2021-11-05 13:30:39 +0300

    routing

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>LOGO</h1>
+    <div>
+      <router-link to="/main">Главная</router-link>
+      <router-link to="/news">Новости</router-link>
+      <router-link to="/about-us">О нас</router-link>
+
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
